5be1e7af39a9e102ce563a0dc14c0efb8bf74199
Gustavo Gunther <Gustavo.gunther@outlook.com>
2025-06-19 22:51:37 -0300

    Update UsuarioRepository.java

--- src/main/java/com/gustavogunter/taskmanager/repository/UsuarioRepository.java
+++ src/main/java/com/gustavogunter/taskmanager/repository/UsuarioRepository.java
@@ -4,6 +4,6 @@ import com.gustavogunter.taskmanager.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Acá podés agregar métodos personalizados si querés buscar por email, etc.
+    
     Usuario findByEmail(String email);
 }
